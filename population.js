5dd2d28d515ab2ce307ae8039f71e6477f8eb6fd
Curtis Mitchell <cmitchell@CMitchell-282.local>
2017-07-29 13:55:16 -0700

    Population density v. income by state

--- population.js
+++ population.js
@@ -1,10 +1,10 @@
 const width = 500;
 const height = 500;
 const padding = 15;
-const xMin = d3.min(states, function(d) { return d.population; });
-const xMax = d3.max(states, function(d) { return d.population; });
-const yMin = d3.min(states, function(d) { return d.electoralVotes; });
-const yMax = d3.max(states, function(d) { return d.electoralVotes; });
+const xMin = d3.min(states, function(d) { return d.population / d.area; });
+const xMax = d3.max(states, function(d) { return d.population / d.area; });
+const yMin = d3.min(states, function(d) { return d.medianIncome; });
+const yMax = d3.max(states, function(d) { return d.medianIncome; });
 const rMin = d3.min(states, function(d) { return d.area; });
 const rMax = d3.max(states, function(d) { return d.area; });
 
@@ -29,6 +29,9 @@ svg.selectAll("circle")
  .data(states)
  .enter()
  .append("circle")
-   .attr("cx", function(d) { return xScale(d.population); })
-   .attr("cy", function(d) { return yScale(d.electoralVotes); })
-   .attr("r", function(d) { return rScale(d.area); });
+   .attr("cx", function(d) { return xScale(d.population / d.area); })
+   .attr("cy", function(d) { return yScale(d.medianIncome); })
+   .attr("r", function(d) { return rScale(d.area); })
+   .on('mouseover', function(d) {
+      return console.log(d.name);
+   });
